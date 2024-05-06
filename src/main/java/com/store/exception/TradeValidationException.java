a6a0fbaf11475fff328c65b0a53247d7c0a94de0
yuktidwivedi <yuktidwivedi03@gmail.com>
2024-05-06 13:11:30 +0530

    Create TradeValidationException.java

---
+++ src/main/java/com/store/exception/TradeValidationException.java
@@ -0,0 +1,10 @@
+package com.store.exception;
+
+public class TradeValidationException extends RuntimeException {
+
+    public TradeValidationException(String message) {
+        super(message);
+    }
+
+    // You can add additional constructors or methods as needed
+}
